7422c4b2774eaaa820166f4ae7f6e060eb2b92c0
Rushi Shah <2016rshah@gmail.com>
2015-04-29 11:15:30 -0400

    Updated port

--- app.js
+++ app.js
@@ -10,5 +10,6 @@
 var express = require('express'); // Express web server framework
 var app = express();
 app.use(express.static(__dirname + '/public'));
-console.log('Listening on 8888');
-app.listen(8888);
+var port = process.env.PORT || 8080;
+console.log("Listening on "+port);
+app.listen(port);
